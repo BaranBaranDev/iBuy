06b8fa0cbf6da867d2606f64d62baadc1d383b7c
BaranBaranDev <auniversty@gmail.com>
2024-08-14 14:09:08 +0300

    Start Project

---
+++ e-commerceApp/e-commerceApp/Controller/MainVC.swift
@@ -0,0 +1,8 @@
+//
+//  MainVC.swift
+//  e-commerceApp
+//
+//  Created by Baran Baran on 14.08.2024.
+//
+
+import Foundation
---
+++ e-commerceApp/e-commerceApp/Scenes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  e-commerceApp
+//
+//  Created by Baran Baran on 14.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
